0ba3f3462959a4582fd17394deea67b556406ad6
fak3alien <alienwhowasfaked@gmail.com>
2022-11-13 20:43:49 +0100

    add express

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { REST, Routes } from "discord.js";
 import { Client, GatewayIntentBits } from "discord.js";
 import { commands, CommandNames } from "./commands";
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
 
@@ -9,32 +10,39 @@ const BOT_TOKEN = process.env.BOT_TOKEN as string;
 const APP_ID = process.env.APPLICATION_ID as string;
 const rest = new REST({ version: "10" }).setToken(BOT_TOKEN);
 
-const registerCommands = async () => {
-  try {
-    console.log("Started refreshing application (/) commands.");
+const app = express();
+const port = 5000;
 
-    await rest.put(Routes.applicationCommands(APP_ID), { body: commands });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 
-    console.log("Successfully reloaded application (/) commands.");
-  } catch (error) {
-    console.error(error);
-  }
-};
+  const registerCommands = async () => {
+    try {
+      console.log("Started refreshing application (/) commands.");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+      await rest.put(Routes.applicationCommands(APP_ID), { body: commands });
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
+      console.log("Successfully reloaded application (/) commands.");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+  });
+
+  client.on("interactionCreate", async (interaction: any) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === CommandNames.PING) {
+      await interaction.reply("I'm alive");
+    }
+  });
+
+  registerCommands()
+    .then(() => client.login(BOT_TOKEN))
+    .catch((err) => console.log(err));
 });
-
-client.on("interactionCreate", async (interaction: any) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === CommandNames.PING) {
-    await interaction.reply("I'm alive");
-  }
-});
-
-registerCommands()
-  .then(() => client.login(BOT_TOKEN))
-  .catch((err) => console.log(err));
